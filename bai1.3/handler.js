80b4739e1cc11ae1930e05ebaa536df7df4d0c2f
thanhson0514 <thanhson0514@gmail.com>
2023-05-29 14:10:37 +0700

    Write a program that takes a number as input and displays its square.

---
+++ bai1.3/handler.js
@@ -0,0 +1,10 @@
+/**
+ * Write a program that takes a number as input and displays its square.
+ * @param {number} num
+ * @returns {number}  the square of number
+ */
+function handler(num) {
+  return num * num;
+}
+
+module.exports = handler;
