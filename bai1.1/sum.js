1e57e5ff8333f938e168fd448e3a361d9b2256e7
thanhson0514 <thanhson0514@gmail.com>
2023-05-29 12:33:15 +0700

    Write a program that takes two numbers as inputs and displays their sum.

---
+++ bai1.1/sum.js
@@ -0,0 +1,10 @@
+/**
+ * Write a program that takes two numbers as inputs and displays their sum
+ * @param {number} a
+ * @param {number} b
+ * @returns sum of a and b
+ */
+function sum(a, b) {
+    return a + b;
+  }
+  module.exports = sum;
